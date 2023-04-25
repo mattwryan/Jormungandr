4870471d4f4da07937232df65494bada5322c1bd
mattwryan <wieserma93@gmail.com>
2023-04-25 10:36:42 -0400

    Fixed some errors with imports

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from turtle import Screen
 from snake import Snake
-from Jormungandr.food import Food
+from food import Food
 from scoreboard import Scoreboard
 import time
 
@@ -26,23 +26,23 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    #Detect collision with food.
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    #Detect collision with wall.
+    # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
 
-    #Detect collision with tail.
+    # Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
-    #if head collides with any segment in the tail
-        #trigger game_over
+    # if head collides with any segment in the tail
+        # trigger game_over
 
 screen.exitonclick()
